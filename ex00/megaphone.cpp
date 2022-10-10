1a738b36c81aef5f63b66518edc90b93561bd5ef
Juyeong Maing <mjy9088@naver.com>
2022-10-10 23:30:24 +0900

    fix ex00

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -2,11 +2,15 @@
 #include <cstddef>
 #include <iostream>
 
+#define DEFAULT_MESSAGE "* LOUD AND UNBEARABLE FEEDBACK NOISE *"
+
 int main(int argc, char **argv) {
-  if (argc < 2)
-    return EXIT_FAILURE;
-  std::string s = argv[1];
-  for (std::size_t i = 0; i < s.length(); i++)
-    std::cout << static_cast<char>(std::toupper(s[i]));
-  std::cout << std::endl;
+  if (argc < 2) {
+    std::cout << DEFAULT_MESSAGE << std::endl;
+  } else {
+    std::string s = argv[1];
+    for (std::size_t i = 0; i < s.length(); i++)
+      std::cout << static_cast<char>(std::toupper(s[i]));
+    std::cout << std::endl;
+  }
 }
