4edff175258932a59dd6ec0662c15a37214586fe
Juyeong Maing <mjy9088@naver.com>
2022-10-12 00:47:21 +0900

    fix compilation error

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -25,12 +25,12 @@ Account::Account(int initial_deposit) {
   this->_nbDeposits = 0;
   this->_nbWithdrawals = 0;
   this->_displayTimestamp();
-  std::cout << "index:" << this->_accountIndex
-            << ";amount:" << this->_nbAccounts << ";created" << std::endl;
+  std::cout << "index:" << this->_accountIndex << ";amount:" << this->_amount
+            << ";created" << std::endl;
 }
 Account::~Account(void) {
-  std::cout << "index:" << this->_accountIndex
-            << ";amount:" << this->_nbAccounts << ";created" << std::endl;
+  std::cout << "index:" << this->_accountIndex << ";amount:" << this->_amount
+            << ";created" << std::endl;
 }
 
 void Account::makeDeposit(int deposit) {
