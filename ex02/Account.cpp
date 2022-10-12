b28b6ab0628babeae595f981e8a820524f710bcc
Juyeong Maing <mjy9088@naver.com>
2022-10-12 21:05:11 +0900

    add _displayTimestamp

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -1,4 +1,6 @@
 #include <ctime>
+#include <iomanip>
+#include <ios>
 #include <iostream>
 
 #include "Account.hpp"
@@ -13,6 +15,7 @@ int Account::getTotalAmount(void) { return totalAmount; }
 int Account::getNbDeposits(void) { return nbDeposits; }
 int Account::getNbWithdrawals(void) { return nbWithdrawals; }
 void Account::displayAccountsInfos(void) {
+  Account::_displayTimestamp();
   std::cout << "accounts:" << Account::getNbAccounts()
             << ";total:" << Account::getTotalAmount()
             << ";deposits:" << Account::getNbDeposits()
@@ -20,15 +23,16 @@ void Account::displayAccountsInfos(void) {
 }
 
 Account::Account(int initial_deposit) {
+  this->_displayTimestamp();
   this->_accountIndex = nbAccounts++;
   totalAmount += this->_amount = initial_deposit;
   this->_nbDeposits = 0;
   this->_nbWithdrawals = 0;
-  this->_displayTimestamp();
   std::cout << "index:" << this->_accountIndex << ";amount:" << this->_amount
             << ";created" << std::endl;
 }
 Account::~Account(void) {
+  this->_displayTimestamp();
   std::cout << "index:" << this->_accountIndex << ";amount:" << this->_amount
             << ";created" << std::endl;
 }
@@ -48,6 +52,7 @@ int Account::checkAmount(void) const {
   return this->_amount - this->_nbWithdrawals;
 }
 void Account::displayStatus(void) const {
+  this->_displayTimestamp();
   std::cout << "index:" << this->_accountIndex << ";amount:" << this->_amount
             << ";deposits:" << this->_nbDeposits
             << ";withdrawals:" << this->_nbWithdrawals << std::endl;
@@ -55,6 +60,13 @@ void Account::displayStatus(void) const {
 
 void Account::_displayTimestamp(void) {
   std::time_t now = std::time(nullptr);
-  std::cout << now << " ";
+  struct tm t = *localtime(&now);
+  std::cout << '[' << std::setw(4) << std::setfill('0') << std::right
+            << t.tm_year + 1900 << std::setw(2) << std::setfill('0')
+            << std::right << t.tm_mon + 1 << std::setw(2) << std::setfill('0')
+            << std::right << t.tm_mday << '_' << std::setw(2)
+            << std::setfill('0') << std::right << t.tm_hour << std::setw(2)
+            << std::setfill('0') << std::right << t.tm_min << std::setw(2)
+            << std::setfill('0') << std::right << t.tm_sec << "] ";
 }
 Account::Account(void) { throw "Not implemented"; }
